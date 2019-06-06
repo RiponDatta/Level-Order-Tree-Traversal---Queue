f0df25077c4c1653d501e508c9884a8de0ea9b70
Ripon Datta <ripondatta.us@gmail.com>
2019-06-06 00:08:29 -0700

    Add project files.

---
+++ Level-Order-Tree-Traversal---Queue/BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level_Order_Tree_Traversal___Queue
+{
+    public class BinaryTree
+    {
+        public Node Root;
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        public void PrintLevelOrder()
+        {
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(Root);
+
+            while(queue.Count != 0)
+            {
+                Node temp = queue.Dequeue();
+                Console.Write(temp.Data + " ");
+
+                if(temp.Left != null)
+                {
+                    queue.Enqueue(temp.Left);
+                }
+
+                if(temp.Right != null)
+                {
+                    queue.Enqueue(temp.Right);
+                }
+            }
+
+        }
+    }
+}
---
+++ Level-Order-Tree-Traversal---Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level_Order_Tree_Traversal___Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new BinaryTree();
+            tree.Root = new Node(1);
+            tree.Root.Left = new Node(2);
+            tree.Root.Right = new Node(3);
+            tree.Root.Left.Left = new Node(4);
+            tree.Root.Left.Right = new Node(5);
+            tree.Root.Right.Right = new Node(7);
+            tree.Root.Right.Right.Right = new Node(15);
+
+            tree.PrintLevelOrder();
+        }
+    }
+}
